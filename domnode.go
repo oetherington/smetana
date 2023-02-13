604a8e22d72dfaefb92e4d6cdb3f65e4c0f75742
Ollie Etherington <ollie@etherington.xyz>
2023-02-13 02:32:29 +0000

    Add missing HTML5 tags

--- domnode.go
+++ domnode.go
@@ -62,6 +62,8 @@ func buildDomNode(tag Tag, args []any) DomNode {
 			node.Children = append(node.Children, value)
 		case Classes:
 			node.Attrs["class"] = Class(node.Attrs["class"], value)
+		case string:
+			node.Children = append(node.Children, Text(value))
 		default:
 			node.appendError(fmt.Errorf("Invalid DomNode argument: %v", arg))
 		}
@@ -69,24 +71,245 @@ func buildDomNode(tag Tag, args []any) DomNode {
 	return node
 }
 
-func Div(args ...any) DomNode {
-	return buildDomNode("div", args)
+func A(href string, args ...any) DomNode {
+	node := buildDomNode("a", args)
+	node.Attrs["href"] = href
+	return node
 }
 
-func Span(args ...any) DomNode {
-	return buildDomNode("span", args)
+func Abbr(args ...any) DomNode {
+	return buildDomNode("abbr", args)
 }
 
-func Head(args ...any) DomNode {
-	return buildDomNode("head", args)
+func Address(args ...any) DomNode {
+	return buildDomNode("address", args)
+}
+
+func Area(args ...any) DomNode {
+	return buildDomNode("area", args)
+}
+
+func Article(args ...any) DomNode {
+	return buildDomNode("article", args)
+}
+
+func Aside(args ...any) DomNode {
+	return buildDomNode("aside", args)
+}
+
+func Audio(args ...any) DomNode {
+	return buildDomNode("audio", args)
+}
+
+func B(args ...any) DomNode {
+	return buildDomNode("b", args)
+}
+
+func Base(href string) DomNode {
+	return DomNode{"base", Attrs{
+		"href":   href,
+		"target": "_blank",
+	}, Children{}, nil}
+}
+
+func Bdi(args ...any) DomNode {
+	return buildDomNode("bdi", args)
+}
+
+func Bdo(args ...any) DomNode {
+	return buildDomNode("bdo", args)
+}
+
+func Blockquote(args ...any) DomNode {
+	return buildDomNode("blockquote", args)
 }
 
 func Body(args ...any) DomNode {
 	return buildDomNode("body", args)
 }
 
-func Title(title string) DomNode {
-	return DomNode{"title", Attrs{}, Children{Text(title)}, nil}
+func Br(args ...any) DomNode {
+	return buildDomNode("br", args)
+}
+
+func Button(args ...any) DomNode {
+	return buildDomNode("button", args)
+}
+
+func Canvas(args ...any) DomNode {
+	return buildDomNode("canvas", args)
+}
+
+func Caption(args ...any) DomNode {
+	return buildDomNode("caption", args)
+}
+
+func Charset(value string) DomNode {
+	if len(value) < 1 {
+		value = "UTF-8"
+	}
+	return DomNode{"meta", Attrs{"charset": value}, Children{}, nil}
+}
+
+func Cite(args ...any) DomNode {
+	return buildDomNode("cite", args)
+}
+
+func Code(args ...any) DomNode {
+	return buildDomNode("code", args)
+}
+
+func Col(args ...any) DomNode {
+	return buildDomNode("col", args)
+}
+
+func Colgroup(args ...any) DomNode {
+	return buildDomNode("colgroup", args)
+}
+
+func Data(args ...any) DomNode {
+	return buildDomNode("data", args)
+}
+
+func Datalist(args ...any) DomNode {
+	return buildDomNode("datalist", args)
+}
+
+func Dd(args ...any) DomNode {
+	return buildDomNode("dd", args)
+}
+
+func Del(args ...any) DomNode {
+	return buildDomNode("del", args)
+}
+
+func Details(args ...any) DomNode {
+	return buildDomNode("details", args)
+}
+
+func Dfn(args ...any) DomNode {
+	return buildDomNode("dfn", args)
+}
+
+func Dialog(args ...any) DomNode {
+	return buildDomNode("dialog", args)
+}
+
+func Div(args ...any) DomNode {
+	return buildDomNode("div", args)
+}
+
+func Dl(args ...any) DomNode {
+	return buildDomNode("dl", args)
+}
+
+func Dt(args ...any) DomNode {
+	return buildDomNode("dt", args)
+}
+
+func Em(args ...any) DomNode {
+	return buildDomNode("em", args)
+}
+
+func Embed(args ...any) DomNode {
+	return buildDomNode("embed", args)
+}
+
+func Fieldset(args ...any) DomNode {
+	return buildDomNode("fieldset", args)
+}
+
+func Figcaption(args ...any) DomNode {
+	return buildDomNode("figcaption", args)
+}
+
+func Figure(args ...any) DomNode {
+	return buildDomNode("figure", args)
+}
+
+func Footer(args ...any) DomNode {
+	return buildDomNode("footer", args)
+}
+
+func Form(args ...any) DomNode {
+	return buildDomNode("form", args)
+}
+
+func H(level int, args ...any) DomNode {
+	tag := fmt.Sprintf("h%d", level)
+	return buildDomNode(tag, args)
+}
+
+func H1(args ...any) DomNode {
+	return buildDomNode("h1", args)
+}
+
+func H2(args ...any) DomNode {
+	return buildDomNode("h2", args)
+}
+
+func H3(args ...any) DomNode {
+	return buildDomNode("h3", args)
+}
+
+func H4(args ...any) DomNode {
+	return buildDomNode("h4", args)
+}
+
+func H5(args ...any) DomNode {
+	return buildDomNode("h5", args)
+}
+
+func H6(args ...any) DomNode {
+	return buildDomNode("h6", args)
+}
+
+func Head(args ...any) DomNode {
+	return buildDomNode("head", args)
+}
+
+func Header(args ...any) DomNode {
+	return buildDomNode("header", args)
+}
+
+func Hr(args ...any) DomNode {
+	return buildDomNode("hr", args)
+}
+
+func I(args ...any) DomNode {
+	return buildDomNode("i", args)
+}
+
+func Iframe(args ...any) DomNode {
+	return buildDomNode("iframe", args)
+}
+
+func Img(args ...any) DomNode {
+	return buildDomNode("img", args)
+}
+
+func Input(args ...any) DomNode {
+	return buildDomNode("input", args)
+}
+
+func Ins(args ...any) DomNode {
+	return buildDomNode("ins", args)
+}
+
+func Kbd(args ...any) DomNode {
+	return buildDomNode("kbd", args)
+}
+
+func Label(args ...any) DomNode {
+	return buildDomNode("label", args)
+}
+
+func Legend(args ...any) DomNode {
+	return buildDomNode("legend", args)
+}
+
+func Li(args ...any) DomNode {
+	return buildDomNode("li", args)
 }
 
 func Link(rel string, href string) DomNode {
@@ -97,11 +320,72 @@ func Link(rel string, href string) DomNode {
 	return DomNode{"link", attrs, Children{}, nil}
 }
 
-func Charset(value string) DomNode {
-	if len(value) < 1 {
-		value = "UTF-8"
-	}
-	return DomNode{"meta", Attrs{"charset": value}, Children{}, nil}
+func Main(args ...any) DomNode {
+	return buildDomNode("main", args)
+}
+
+func Map(args ...any) DomNode {
+	return buildDomNode("map", args)
+}
+
+func Mark(args ...any) DomNode {
+	return buildDomNode("mark", args)
+}
+
+func Meter(args ...any) DomNode {
+	return buildDomNode("meter", args)
+}
+
+func Nav(args ...any) DomNode {
+	return buildDomNode("nav", args)
+}
+
+func Noscript(args ...any) DomNode {
+	return buildDomNode("noscript", args)
+}
+
+func Object(args ...any) DomNode {
+	return buildDomNode("object", args)
+}
+
+func Ol(args ...any) DomNode {
+	return buildDomNode("ol", args)
+}
+
+func Optgroup(args ...any) DomNode {
+	return buildDomNode("optgroup", args)
+}
+
+func Option(args ...any) DomNode {
+	return buildDomNode("option", args)
+}
+
+func Output(args ...any) DomNode {
+	return buildDomNode("output", args)
+}
+
+func P(args ...any) DomNode {
+	return buildDomNode("p", args)
+}
+
+func Param(args ...any) DomNode {
+	return buildDomNode("param", args)
+}
+
+func Picture(args ...any) DomNode {
+	return buildDomNode("picture", args)
+}
+
+func Pre(args ...any) DomNode {
+	return buildDomNode("pre", args)
+}
+
+func Progress(args ...any) DomNode {
+	return buildDomNode("progress", args)
+}
+
+func Q(args ...any) DomNode {
+	return buildDomNode("q", args)
 }
 
 func Refresh(value uint) DomNode {
@@ -111,21 +395,142 @@ func Refresh(value uint) DomNode {
 	}, Children{}, nil}
 }
 
-func Base(href string) DomNode {
-	return DomNode{"base", Attrs{
-		"href":   href,
-		"target": "_blank",
-	}, Children{}, nil}
+func Rp(args ...any) DomNode {
+	return buildDomNode("rp", args)
 }
 
-func Script(src string) DomNode {
+func Rt(args ...any) DomNode {
+	return buildDomNode("rt", args)
+}
+
+func Ruby(args ...any) DomNode {
+	return buildDomNode("ruby", args)
+}
+
+func S(args ...any) DomNode {
+	return buildDomNode("s", args)
+}
+
+func Samp(args ...any) DomNode {
+	return buildDomNode("samp", args)
+}
+
+func Script(args ...any) DomNode {
+	return buildDomNode("script", args)
+}
+
+func ScriptSrc(src string) DomNode {
 	return DomNode{"script", Attrs{"src": src}, Children{}, nil}
 }
 
-func InlineScript(value string) DomNode {
-	return DomNode{"script", Attrs{}, Children{Text(value)}, nil}
+func Section(args ...any) DomNode {
+	return buildDomNode("section", args)
 }
 
-func Style(value string) DomNode {
-	return DomNode{"style", Attrs{}, Children{Text(value)}, nil}
+func Select(args ...any) DomNode {
+	return buildDomNode("select", args)
+}
+
+func Small(args ...any) DomNode {
+	return buildDomNode("small", args)
+}
+
+func Source(args ...any) DomNode {
+	return buildDomNode("source", args)
+}
+
+func Span(args ...any) DomNode {
+	return buildDomNode("span", args)
+}
+
+func Strong(args ...any) DomNode {
+	return buildDomNode("strong", args)
+}
+
+func Style(args ...any) DomNode {
+	return buildDomNode("style", args)
+}
+
+func Sub(args ...any) DomNode {
+	return buildDomNode("sub", args)
+}
+
+func Summary(args ...any) DomNode {
+	return buildDomNode("summary", args)
+}
+
+func Sup(args ...any) DomNode {
+	return buildDomNode("sup", args)
+}
+
+func Svg(args ...any) DomNode {
+	return buildDomNode("svg", args)
+}
+
+func Table(args ...any) DomNode {
+	return buildDomNode("table", args)
+}
+
+func Tbody(args ...any) DomNode {
+	return buildDomNode("tbody", args)
+}
+
+func Td(args ...any) DomNode {
+	return buildDomNode("td", args)
+}
+
+func Template(args ...any) DomNode {
+	return buildDomNode("template", args)
+}
+
+func Textarea(args ...any) DomNode {
+	return buildDomNode("textarea", args)
+}
+
+func Tfoot(args ...any) DomNode {
+	return buildDomNode("tfoot", args)
+}
+
+func Th(args ...any) DomNode {
+	return buildDomNode("th", args)
+}
+
+func Thead(args ...any) DomNode {
+	return buildDomNode("thead", args)
+}
+
+func Time(args ...any) DomNode {
+	return buildDomNode("time", args)
+}
+
+func Title(title string) DomNode {
+	return DomNode{"title", Attrs{}, Children{Text(title)}, nil}
+}
+
+func Tr(args ...any) DomNode {
+	return buildDomNode("tr", args)
+}
+
+func Track(args ...any) DomNode {
+	return buildDomNode("track", args)
+}
+
+func U(args ...any) DomNode {
+	return buildDomNode("u", args)
+}
+
+func Ul(args ...any) DomNode {
+	return buildDomNode("ul", args)
+}
+
+func Var(args ...any) DomNode {
+	return buildDomNode("var", args)
+}
+
+func Video(args ...any) DomNode {
+	return buildDomNode("video", args)
+}
+
+func Wbr(args ...any) DomNode {
+	return buildDomNode("wbr", args)
 }
--- domnode_test.go
+++ domnode_test.go
@@ -31,87 +31,15 @@ func TestRenderDomNodeWithSingleChild(t *testing.T) {
 }
 
 func TestRenderDomNodeWithClasses(t *testing.T) {
+	node := buildDomNode("div", []any{"foo"})
+	result := RenderOpts(node, true, nil)
+	assertEqual(t, "<div>foo</div>", result)
+}
+
+func TestRenderDomNodeWithStrings(t *testing.T) {
 	node := buildDomNode("div", []any{Classes{"a": true}})
 	result := RenderOpts(node, true, nil)
 	assertEqual(t, "<div class=\"a\" />", result)
-}
-
-func TestRenderDiv(t *testing.T) {
-	node := Div(Attrs{"class": "foo"}, Children{Text("bar")})
-	result := RenderOpts(node, true, nil)
-	assertEqual(t, "<div class=\"foo\">bar</div>", result)
-}
-
-func TestRenderSpan(t *testing.T) {
-	node := Span(Attrs{"class": "foo"}, Children{Text("bar")})
-	result := RenderOpts(node, true, nil)
-	assertEqual(t, "<span class=\"foo\">bar</span>", result)
-}
-
-func TestRenderHead(t *testing.T) {
-	node := Head(Attrs{"class": "foo"}, Children{Text("bar")})
-	result := RenderOpts(node, true, nil)
-	assertEqual(t, "<head class=\"foo\">bar</head>", result)
-}
-
-func TestRenderBody(t *testing.T) {
-	node := Body(Attrs{"class": "foo"}, Children{Text("bar")})
-	result := RenderOpts(node, true, nil)
-	assertEqual(t, "<body class=\"foo\">bar</body>", result)
-}
-
-func TestRenderTitle(t *testing.T) {
-	node := Title("hello world")
-	result := RenderOpts(node, true, nil)
-	assertEqual(t, "<title>hello world</title>", result)
-}
-
-func TestRenderLink(t *testing.T) {
-	node := Link("stylesheet", "/main.css")
-	result := RenderOpts(node, true, nil)
-	assertEqual(t, "<link href=\"/main.css\" rel=\"stylesheet\" />", result)
-}
-
-func TestRenderCharset(t *testing.T) {
-	node := Charset("ASCII")
-	result := RenderOpts(node, true, nil)
-	assertEqual(t, "<meta charset=\"ASCII\" />", result)
-}
-
-func TestRenderCharsetDefault(t *testing.T) {
-	node := Charset("")
-	result := RenderOpts(node, true, nil)
-	assertEqual(t, "<meta charset=\"UTF-8\" />", result)
-}
-
-func TestRenderRefresh(t *testing.T) {
-	node := Refresh(30)
-	result := RenderOpts(node, true, nil)
-	assertEqual(t, "<meta content=\"30\" http-equiv=\"refresh\" />", result)
-}
-
-func TestRenderBase(t *testing.T) {
-	node := Base("https://example.com/")
-	result := RenderOpts(node, true, nil)
-	assertEqual(t, "<base href=\"https://example.com/\" target=\"_blank\" />", result)
-}
-
-func TestRenderScript(t *testing.T) {
-	node := Script("/main.js")
-	result := RenderOpts(node, true, nil)
-	assertEqual(t, "<script src=\"/main.js\" />", result)
-}
-
-func TestRenderInlineScript(t *testing.T) {
-	node := InlineScript("alert('foo')")
-	result := RenderOpts(node, true, nil)
-	assertEqual(t, "<script>alert('foo')</script>", result)
-}
-
-func TestRenderStyle(t *testing.T) {
-	node := Style("body{background:red}")
-	result := RenderOpts(node, true, nil)
-	assertEqual(t, "<style>body{background:red}</style>", result)
 }
 
 func TestDomNodeReportsErrors(t *testing.T) {
@@ -122,4 +50,155 @@ func TestDomNodeReportsErrors(t *testing.T) {
 	assertEqual(t, "<div />", result)
 	output := strings.TrimSpace(target.String())
 	assertEqual(t, "Invalid DomNode argument: 3", output)
+}
+
+type DomNodeTest struct {
+	node     DomNode
+	expected string
+}
+
+func TestRenderIndividualDomNodes(t *testing.T) {
+	testCases := []DomNodeTest{
+		{
+			A("/index.html", Text("Home")),
+			"<a href=\"/index.html\">Home</a>",
+		},
+		{Abbr(Text("foo")), "<abbr>foo</abbr>"},
+		{Address(Text("foo")), "<address>foo</address>"},
+		{Area(Text("foo")), "<area>foo</area>"},
+		{Article(Text("foo")), "<article>foo</article>"},
+		{Aside(Text("foo")), "<aside>foo</aside>"},
+		{Audio(Text("foo")), "<audio>foo</audio>"},
+		{B(Text("foo")), "<b>foo</b>"},
+		{
+			Base("https://example.com/"),
+			"<base href=\"https://example.com/\" target=\"_blank\" />",
+		},
+		{Bdi(Text("foo")), "<bdi>foo</bdi>"},
+		{Bdo(Text("foo")), "<bdo>foo</bdo>"},
+		{Blockquote(Text("foo")), "<blockquote>foo</blockquote>"},
+		{Br(), "<br />"},
+		{Button(Text("foo")), "<button>foo</button>"},
+		{Canvas(Text("foo")), "<canvas>foo</canvas>"},
+		{Caption(Text("foo")), "<caption>foo</caption>"},
+		{
+			Body(Attrs{"class": "foo"}, Children{Text("bar")}),
+			"<body class=\"foo\">bar</body>",
+		},
+		{Charset("ASCII"), "<meta charset=\"ASCII\" />"},
+		{Charset(""), "<meta charset=\"UTF-8\" />"},
+		{Cite(Text("foo")), "<cite>foo</cite>"},
+		{Code(Text("foo")), "<code>foo</code>"},
+		{Col(Text("foo")), "<col>foo</col>"},
+		{Colgroup(Text("foo")), "<colgroup>foo</colgroup>"},
+		{Data(Text("foo")), "<data>foo</data>"},
+		{Datalist(Text("foo")), "<datalist>foo</datalist>"},
+		{Dd(Text("foo")), "<dd>foo</dd>"},
+		{Del(Text("foo")), "<del>foo</del>"},
+		{Details(Text("foo")), "<details>foo</details>"},
+		{Dfn(Text("foo")), "<dfn>foo</dfn>"},
+		{Dialog(Text("foo")), "<dialog>foo</dialog>"},
+		{
+			Div(Attrs{"class": "foo"}, Children{Text("bar")}),
+			"<div class=\"foo\">bar</div>",
+		},
+		{Dl(Text("foo")), "<dl>foo</dl>"},
+		{Dt(Text("foo")), "<dt>foo</dt>"},
+		{Em(Text("foo")), "<em>foo</em>"},
+		{Embed(Text("foo")), "<embed>foo</embed>"},
+		{Fieldset(Text("foo")), "<fieldset>foo</fieldset>"},
+		{Figcaption(Text("foo")), "<figcaption>foo</figcaption>"},
+		{Figure(Text("foo")), "<figure>foo</figure>"},
+		{Footer(Text("foo")), "<footer>foo</footer>"},
+		{Form(Text("foo")), "<form>foo</form>"},
+		{H(1, Text("foo")), "<h1>foo</h1>"},
+		{H(2, Text("foo")), "<h2>foo</h2>"},
+		{H(3, Text("foo")), "<h3>foo</h3>"},
+		{H(4, Text("foo")), "<h4>foo</h4>"},
+		{H(5, Text("foo")), "<h5>foo</h5>"},
+		{H(6, Text("foo")), "<h6>foo</h6>"},
+		{H1(Text("foo")), "<h1>foo</h1>"},
+		{H2(Text("foo")), "<h2>foo</h2>"},
+		{H3(Text("foo")), "<h3>foo</h3>"},
+		{H4(Text("foo")), "<h4>foo</h4>"},
+		{H5(Text("foo")), "<h5>foo</h5>"},
+		{H6(Text("foo")), "<h6>foo</h6>"},
+		{Head(Text("foo")), "<head>foo</head>"},
+		{Header(Text("foo")), "<header>foo</header>"},
+		{Hr(Text("foo")), "<hr>foo</hr>"},
+		{I(Text("foo")), "<i>foo</i>"},
+		{Iframe(Text("foo")), "<iframe>foo</iframe>"},
+		{Img(Text("foo")), "<img>foo</img>"},
+		{Input(Text("foo")), "<input>foo</input>"},
+		{Ins(Text("foo")), "<ins>foo</ins>"},
+		{Kbd(Text("foo")), "<kbd>foo</kbd>"},
+		{Label(Text("foo")), "<label>foo</label>"},
+		{Legend(Text("foo")), "<legend>foo</legend>"},
+		{Li(Text("foo")), "<li>foo</li>"},
+		{
+			Link("stylesheet", "/main.css"),
+			"<link href=\"/main.css\" rel=\"stylesheet\" />",
+		},
+		{Main(Text("foo")), "<main>foo</main>"},
+		{Map(Text("foo")), "<map>foo</map>"},
+		{Mark(Text("foo")), "<mark>foo</mark>"},
+		{Meter(Text("foo")), "<meter>foo</meter>"},
+		{Nav(Text("foo")), "<nav>foo</nav>"},
+		{Noscript(Text("foo")), "<noscript>foo</noscript>"},
+		{Object(Text("foo")), "<object>foo</object>"},
+		{Ol(Text("foo")), "<ol>foo</ol>"},
+		{Optgroup(Text("foo")), "<optgroup>foo</optgroup>"},
+		{Option(Text("foo")), "<option>foo</option>"},
+		{Output(Text("foo")), "<output>foo</output>"},
+		{P(Text("foo")), "<p>foo</p>"},
+		{Param(Text("foo")), "<param>foo</param>"},
+		{Picture(Text("foo")), "<picture>foo</picture>"},
+		{Pre(Text("foo")), "<pre>foo</pre>"},
+		{Progress(Text("foo")), "<progress>foo</progress>"},
+		{Q(Text("foo")), "<q>foo</q>"},
+		{
+			Refresh(30),
+			"<meta content=\"30\" http-equiv=\"refresh\" />",
+		},
+		{Rp(Text("foo")), "<rp>foo</rp>"},
+		{Rt(Text("foo")), "<rt>foo</rt>"},
+		{Ruby(Text("foo")), "<ruby>foo</ruby>"},
+		{S(Text("foo")), "<s>foo</s>"},
+		{Samp(Text("foo")), "<samp>foo</samp>"},
+		{Script("alert('foo')"), "<script>alert('foo')</script>"},
+		{ScriptSrc("/main.js"), "<script src=\"/main.js\" />"},
+		{Section(Text("foo")), "<section>foo</section>"},
+		{Select(Text("foo")), "<select>foo</select>"},
+		{Small(Text("foo")), "<small>foo</small>"},
+		{Source(Text("foo")), "<source>foo</source>"},
+		{Span(Text("foo")), "<span>foo</span>"},
+		{Strong(Text("foo")), "<strong>foo</strong>"},
+		{Style("body{background:red}"), "<style>body{background:red}</style>"},
+		{Sub(Text("foo")), "<sub>foo</sub>"},
+		{Summary(Text("foo")), "<summary>foo</summary>"},
+		{Sup(Text("foo")), "<sup>foo</sup>"},
+		{Svg(Text("foo")), "<svg>foo</svg>"},
+		{Table(Text("foo")), "<table>foo</table>"},
+		{Tbody(Text("foo")), "<tbody>foo</tbody>"},
+		{Td(Text("foo")), "<td>foo</td>"},
+		{Template(Text("foo")), "<template>foo</template>"},
+		{Textarea(Text("foo")), "<textarea>foo</textarea>"},
+		{Tfoot(Text("foo")), "<tfoot>foo</tfoot>"},
+		{Th(Text("foo")), "<th>foo</th>"},
+		{Thead(Text("foo")), "<thead>foo</thead>"},
+		{Time(Text("foo")), "<time>foo</time>"},
+		{Title("hello world"), "<title>hello world</title>"},
+		{Tr(Text("foo")), "<tr>foo</tr>"},
+		{Track(Text("foo")), "<track>foo</track>"},
+		{U(Text("foo")), "<u>foo</u>"},
+		{Ul(Text("foo")), "<ul>foo</ul>"},
+		{Var(Text("foo")), "<var>foo</var>"},
+		{Video(Text("foo")), "<video>foo</video>"},
+		{Wbr(Text("foo")), "<wbr>foo</wbr>"},
+	}
+
+	for _, testCase := range testCases {
+		result := RenderOpts(testCase.node, true, nil)
+		assertEqual(t, testCase.expected, result)
+	}
 }
