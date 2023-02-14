83c500706bb2eb04b26fd5a9febf0a5eafcf86f8
Ollie Etherington <ollie@etherington.xyz>
2023-02-14 22:42:25 +0000

    Add doc comment for metanode

--- metanode.go
+++ metanode.go
@@ -1,10 +1,15 @@
 package smetana
 
+/*
+ * A `meta` node in an HTML document to associate some `content`
+ * value to a particular `name`.
+ */
 type MetaNode struct {
 	Name    string
 	Content string
 }
 
+// Convert a MetaNode to HTML
 func (node MetaNode) ToHtml(builder *Builder) {
 	builder.writeShortTag("meta", Attrs{
 		"name":    node.Name,
@@ -12,22 +17,28 @@ func (node MetaNode) ToHtml(builder *Builder) {
 	})
 }
 
+// Create a generic meta node with the given name and content
 func Meta(name string, content string) MetaNode {
 	return MetaNode{name, content}
 }
 
+// Create a `keywords` meta node with the given value
 func Keywords(value string) MetaNode {
 	return MetaNode{"keywords", value}
 }
 
+// Create a `description` meta node with the given value
 func Description(value string) MetaNode {
 	return MetaNode{"description", value}
 }
 
+// Create an `author` meta node with the given value
 func Author(value string) MetaNode {
 	return MetaNode{"author", value}
 }
 
+// Create a `viewport` meta node with the given value, or, if the
+// empty string is provided, the default "width=device-width, initial-scale=1.0"
 func Viewport(value string) MetaNode {
 	if len(value) < 1 {
 		value = "width=device-width, initial-scale=1.0"
