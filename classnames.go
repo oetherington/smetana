90b67c5d70d597e2d4f1782ea6a88b4776ff4322
Ollie Etherington <ollie@etherington.xyz>
2023-02-14 22:03:45 +0000

    Add doc comments for classnames

--- classnames.go
+++ classnames.go
@@ -2,8 +2,23 @@ package smetana
 
 import "strings"
 
+/*
+ * A map of conditional classes to be passed to ClassNames. The keys
+ * are class names and the values are booleans indicating whether or
+ * not to include that class name. For instance, {"foo": true, "bar"
+ * false} will evaluate to "foo"
+ */
 type Classes map[ClassName]bool
 
+/*
+ * A utility function for concatenating multiple class names into a
+ * single string suitable for embedding in HTML. Arguments may be of
+ * several different types:
+ *  - string
+ *  - ClassName
+ *  - Classes
+ * Arguments of other types are ignored
+ */
 func ClassNames(args ...any) string {
 	classes := []string{}
 	for _, arg := range args {
