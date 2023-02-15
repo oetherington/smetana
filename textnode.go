bba5a06250023b8fc08b3b43349424ef642527cb
Ollie Etherington <ollie@etherington.xyz>
2023-02-15 00:24:17 +0000

    Add doc string for TextNode

--- textnode.go
+++ textnode.go
@@ -1,13 +1,16 @@
 package smetana
 
+// A Node representing raw text without any surrounding tag
 type TextNode struct {
 	Text string
 }
 
+// Convert a TextNode to HTML
 func (node TextNode) ToHtml(builder *Builder) {
 	builder.Buf.WriteString(node.Text)
 }
 
+// Create a TextNode from the given string
 func Text(text string) TextNode {
 	return TextNode{text}
 }
